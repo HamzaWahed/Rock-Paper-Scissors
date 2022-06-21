5c9ab241219134cddb7dea745d8f8a6028a427c1
Hamza Wahed <hamzawahed@dal.ca>
2022-06-21 16:34:37 -0300

    add function for computer choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
-function computerPlay{
-    
+const options = ["Rock","Paper","Scissors"];
+
+function computerPlay(){
+    let choice = Math.floor(Math.random()*options.length);
+    console.log(options[choice]);
 }
