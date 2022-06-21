c50a2a9e062ee4173dbb585d78e4e06d7652205b
Hamza Wahed <hamzawahed@dal.ca>
2022-06-21 16:47:00 -0300

    add function to play a single round

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
-const options = ["Rock","Paper","Scissors"];
+const options = ["rock","paper","scissors"];
 
 function computerPlay(){
     let choice = Math.floor(Math.random()*options.length);
-    console.log(options[choice]);
+    return options[choice];
+}
+
+function playRound(playerSelection, computerSelection){
+    let computerChoice = computerSelection.toLowerCase();
+    let playerChoice = playerSelection.toLowerCase();
+    if((computerChoice===options[0] && playerChoice===options[2])
+    || (computerChoice===options[1] && playerChoice===options[0])
+    || (computerChoice===options[2] && playerChoice===options[1])){
+        console.log("You Lose! " + computerChoice + " beats " + playerChoice);
+    }else{
+        console.log("You Win! " + playerChoice + " beats " + computerChoice);
+    }
 }
